37b70be78c92ee678231deeb462f2417c72b65be
mlypai <balladbell@gmail.com>
2023-01-14 07:10:35 +0200

    Aligning grid inside container

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const container = document.getElementById('sheet');
 let gridSize = 16;
 
+
 for(let i = 0; i < gridSize; i++)
 {
-    console.log("here");
     var grid = document.createElement('div');
+
+    grid.style.height = `${100 / gridSize}%`;
     for(let j = 0; j < gridSize; j++)
     {
         var elem =document.createElement('div');
+        elem.style.width = `${100 / gridSize}%`;
         elem.classList.add('grid');
 
         grid.appendChild(elem);
