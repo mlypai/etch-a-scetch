c5da620c4911ed4708e2e0f4bd90d4e77ae9f512
mlypai <balladbell@gmail.com>
2023-02-24 19:50:57 +0200

    adding shade function

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let gridSize = document.getElementById('size');
 var mode = document.querySelectorAll('.mode');
 const black = document.getElementById('black');
 const color = document.getElementById('color');
-const shadow = document.getElementById('shadow');
+const shade = document.getElementById('shade');
 const rainbow = document.getElementById('rainbow');
 const clear = document.getElementById('clear');
 
@@ -20,11 +20,12 @@ function createGrid(){
         elem.classList.add('grid');
         elem.style.height = `${100 / gridSize.value}%`;
         elem.style.width = `${100 / gridSize.value}%`;
+        elem.style.backgroundColor = 'rgb(255, 255, 255)';
         container.appendChild(elem);
     }
     grid = document.querySelectorAll('.grid');
     grid.forEach(a => a.addEventListener('mouseover', () => paint(a)));
-    clear.addEventListener('click', () => grid.forEach(a => a.style.backgroundColor = 'white'));
+    clear.addEventListener('click', () => grid.forEach(a => a.style.backgroundColor = 'rgb(255, 255, 255)'));
 }
 
 if(!grid)
@@ -37,11 +38,16 @@ gridSize.addEventListener('change', () =>
 
 function paint(grid){
     if (black.classList.contains('active'))
-        grid.style.backgroundColor = 'black';
+        grid.style.backgroundColor = 'rgb(0,0,0)';
     if(color.classList.contains('active'))
         grid.style.backgroundColor = colorPicker.value;
     if(rainbow.classList.contains('active'))
         grid.style.backgroundColor = `rgb(${getRand()}, ${getRand()}, ${getRand()})`;
+    if(shade.classList.contains('active'))
+    {
+        var rgb = grid.style.backgroundColor.replace(/[a-z]|\(|\)|\s/g, "").split(',');
+        grid.style.backgroundColor = `rgb(${rgb[0] -25}, ${rgb[1] -25}, ${rgb[2] -25})`;
+    }
 }
 
 function getRand()
